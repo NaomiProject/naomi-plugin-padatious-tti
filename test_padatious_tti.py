73d84869dd7ae2cf217717fe79f0d904b8211737
Aryan Yadav <141573833+aryan0931@users.noreply.github.com>
2024-10-04 12:53:34 +0530

    some changes

--- test_padatious_tti.py
+++ test_padatious_tti.py
@@ -1,1 +1,9 @@
-test_padatious_tti.py
+# -*- coding: utf-8 -*-
+import unittest
+from naomi import testutils
+from . import padatious_tti
+
+
+class TestPadatiousTTIPlugin(unittest.TestCase):
+    def setUp(self):
+        self.plugin = testutils.get_plugin_instance(padatious_tti.PadatiousTTIPlugin)
